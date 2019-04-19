633cf63998c6dca258d3a1ead7d1b0935b19fbc5
MNTMDEV <44183946+MNTMDEV@users.noreply.github.com>
2019-04-19 17:49:04 +0800

    Add files via upload

---
+++ HitCrack/HitCrack/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace HitCrack
+{
+    partial class HitCrack
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblDeclare = new System.Windows.Forms.Label();
+            this.txtCnt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAval = new System.Windows.Forms.TextBox();
+            this.txtBval = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtGid = new System.Windows.Forms.TextBox();
+            this.txtCid = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtOpt = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(50, 225);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(273, 60);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "Start Flooding";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(47, 29);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(47, 15);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "Ready";
+            // 
+            // lblDeclare
+            // 
+            this.lblDeclare.Location = new System.Drawing.Point(49, 306);
+            this.lblDeclare.Name = "lblDeclare";
+            this.lblDeclare.Size = new System.Drawing.Size(493, 45);
+            this.lblDeclare.TabIndex = 2;
+            this.lblDeclare.Text = "The tool is only for test.Any forms of abusing is prohibited.";
+            // 
+            // txtCnt
+            // 
+            this.txtCnt.Location = new System.Drawing.Point(152, 180);
+            this.txtCnt.Name = "txtCnt";
+            this.txtCnt.Size = new System.Drawing.Size(171, 25);
+            this.txtCnt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Counts";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "http://A.mikecrm.com/B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "A";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "B";
+            // 
+            // txtAval
+            // 
+            this.txtAval.Location = new System.Drawing.Point(152, 96);
+            this.txtAval.Name = "txtAval";
+            this.txtAval.Size = new System.Drawing.Size(171, 25);
+            this.txtAval.TabIndex = 9;
+            // 
+            // txtBval
+            // 
+            this.txtBval.Location = new System.Drawing.Point(152, 134);
+            this.txtBval.Name = "txtBval";
+            this.txtBval.Size = new System.Drawing.Size(171, 25);
+            this.txtBval.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "CPGID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(341, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 15);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "CPCID";
+            // 
+            // txtGid
+            // 
+            this.txtGid.Location = new System.Drawing.Point(442, 96);
+            this.txtGid.Name = "txtGid";
+            this.txtGid.Size = new System.Drawing.Size(100, 25);
+            this.txtGid.TabIndex = 13;
+            // 
+            // txtCid
+            // 
+            this.txtCid.Location = new System.Drawing.Point(442, 134);
+            this.txtCid.Name = "txtCid";
+            this.txtCid.Size = new System.Drawing.Size(100, 25);
+            this.txtCid.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(341, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Option Text";
+            // 
+            // txtOpt
+            // 
+            this.txtOpt.Location = new System.Drawing.Point(442, 179);
+            this.txtOpt.Name = "txtOpt";
+            this.txtOpt.Size = new System.Drawing.Size(100, 25);
+            this.txtOpt.TabIndex = 16;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(344, 225);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(198, 60);
+            this.btnFind.TabIndex = 17;
+            this.btnFind.Text = "Find Parameter";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // HitCrack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 377);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtOpt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtCid);
+            this.Controls.Add(this.txtGid);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBval);
+            this.Controls.Add(this.txtAval);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCnt);
+            this.Controls.Add(this.lblDeclare);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "HitCrack";
+            this.Text = "MikeCRM Vote Flood Tool(Release Edition)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblDeclare;
+        private System.Windows.Forms.TextBox txtCnt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAval;
+        private System.Windows.Forms.TextBox txtBval;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtGid;
+        private System.Windows.Forms.TextBox txtCid;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtOpt;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ HitCrack/HitCrack/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace HitCrack
+{
+    public partial class HitCrack : Form
+    {
+        public HitCrack()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            aval=txtAval.Text;
+            bval = txtBval.Text;
+            cpgid = txtGid.Text;
+            cpcid = txtCid.Text;
+            if ((cpgid == "") || (cpcid == ""))
+            {
+                MessageBox.Show("参数cpgid cpcid不能为空");
+                return;
+            }
+            GeneratePara();
+            //return;
+            tt = Convert.ToInt32(txtCnt.Text);
+            cnt = 0;
+            cfin = 0;
+            cwork = 0;
+            btnExecute.Enabled = false;
+            for (int i = 0; i<100 ; i++)
+            {
+                Thread th = new Thread(new ThreadStart(WebThread));
+                th.IsBackground = true;
+                th.Start();
+            }
+        }
+
+        private void GeneratePara()
+        {
+            string orn = HttpPost.GetContent("http://" + aval + ".mikecrm.com/" + bval);
+            string ctt = orn.ToString();
+            string i,s,acc;
+            //string cp,cid,cval;
+            Regex reg = new Regex("var SOUL = (.+);");
+            Match mt=reg.Match(ctt);
+            ctt=mt.Groups[1].Value;
+            reg = new Regex("\"I\":(\\w*)");
+            mt = reg.Match(ctt);
+            i = mt.Groups[1].Value;
+            reg = new Regex("\"FRS\":(\\w*)");
+            mt = reg.Match(ctt);
+            s = mt.Groups[1].Value;
+            reg = new Regex("\"ACC\":\"(\\w*)\"");
+            mt = reg.Match(ctt);
+            acc = mt.Groups[1].Value;
+            //reg = new Regex("\"cp\":{(.+)},");
+            //mt = reg.Match(ctt);
+            //cp = mt.Groups[1].Value;
+            
+            //reg = new Regex("\\[(\\w*),\"1\"\\]");
+            //mt = reg.Match(cp);
+            //cid=mt.Groups[1].Value;
+            //reg = new Regex("\""+cid+"\":(\\w*)");
+            //mt = reg.Match(cp);
+            //cval = mt.Groups[1].Value; //cp value
+
+            //reg = new Regex("\\$c(\\w*).1.0.0:\\$item0.0.1.0\">1");
+            //mt = reg.Match(orn);
+            //lblResult.Text = mt.Groups[1].Value;
+            dval = "{\"cvs\":{\"i\":" + i + ",\"t\":\"" + bval + "\",\"s\":" + s + ",\"acc\":\"" + acc + "\",\"r\":\"\",\"c\":{\"cp\":{\""+cpgid+"\":"+cpcid+"}}}}";
+            dval = HttpUtility.UrlEncode(dval, Encoding.UTF8);
+        }
+        
+        private void WebThread()
+        {
+            mwork.WaitOne();
+            cwork++;
+            mwork.ReleaseMutex();
+            while (true)
+            {
+                //是否增加已完成个数
+                mutex.WaitOne();
+                if (cnt == tt)
+                {
+                    mwork.WaitOne();
+                    cwork--;
+                    if(cwork==0)
+                        this.btnExecute.BeginInvoke(new MethodInvoker(() => { MessageBox.Show("Complete", "Progress"); btnExecute.Enabled = true; }));
+                    mwork.ReleaseMutex();
+                    
+                    mutex.ReleaseMutex();
+                    return;
+                }
+                cnt++;
+                mutex.ReleaseMutex();
+                
+                try
+                {
+                    string getstr = HttpPost.Get("http://" + aval + ".mikecrm.com/" + bval);
+                    string fdata = "d=" + dval;
+                    HttpPost.Post("http://" + aval + ".mikecrm.com/handler/web/form_runtime/handleSubmit.php", fdata, getstr);
+                }
+                catch (Exception)
+                {
+                }
+                mfin.WaitOne();
+                cfin++;
+                this.lblResult.BeginInvoke(new MethodInvoker(() => { lblResult.Text = "已经完成任务" + cfin.ToString() + "个" + ",共" + tt.ToString() + "个"; }));
+                mfin.ReleaseMutex();
+            }
+        }
+        static int cnt; //载入+完成的任务
+        static int cfin; //完成的
+        static int cwork; //工作的
+        static Mutex mutex = new Mutex();
+        static Mutex mfin = new Mutex();
+        static Mutex mwork = new Mutex();
+        static int tt;
+        static string dval="";
+        static string aval = "";
+        static string bval = "";
+        static string cpgid = "";
+        static string cpcid = "";
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string ctt = HttpPost.GetContent("http://" + txtAval.Text + ".mikecrm.com/" + txtBval.Text);
+            Regex reg = new Regex("var SOUL = (.+);");
+            Match mt = reg.Match(ctt);
+            MatchCollection mtc;
+            ctt = mt.Groups[1].Value;
+
+            //获取所有控件组
+            reg = new Regex(@"""cpo"":""([0-9;]*)""");
+            mt=reg.Match(ctt);
+            string ctlg = mt.Groups[1].Value;
+
+            //每组遍历控件
+            reg = new Regex(@"([0-9]+)");
+            mtc = reg.Matches(ctlg);
+            foreach(Match mte in mtc)
+            {
+                //其中一个控件组
+                string strgid = mte.Groups[1].Value;
+                reg = new Regex(@"(""" + strgid + @""":\[\d,\d,\{""l"":\[((\[([0-9]*),""([^""]*)""\],)*)(\[([0-9]*),""([^""]*)""\]?)\],""df"":\[\],""t"":""([^""]*)""((,""d"":"""",""ts"":null,""ds"":null)?),""l2s"":\{((""([0-9]*)"":([0-9]*),)*)((""([0-9]*)"":([0-9]*))?)\}\},{""rwh"":\d},\d])");
+                mt = reg.Match(ctt);
+                string strg=mt.Groups[1].Value;
+
+                //获取标题
+                reg = new Regex(@"""t"":""([^""]*)""");
+                mt = reg.Match(strg);
+                string gti=mt.Groups[1].Value; //标题
+
+                //获取所有符合要求的控件
+                reg = new Regex(@"\[([0-9]*),"""+txtOpt.Text+@"""\]");
+                mtc=reg.Matches(strg);
+                foreach(Match mc in mtc)
+                {
+                    //其中一个符合要求的控件
+                    string scId = mc.Groups[1].Value;
+                    reg = new Regex(@""""+scId+@""":([0-9]*)");
+                    mt = reg.Match(strg);
+                    string cId = mt.Groups[1].Value;
+                    DialogResult dres=MessageBox.Show("Find A Result:\nTitle of Control Group:" + gti +"\nClick YES to Confirm,Click NO to Find Next,Click CANCEL to Stop Searching", "Search Result", MessageBoxButtons.YesNoCancel);
+                    if (dres == DialogResult.Yes)
+                    {
+                        //设置文本框的文本
+                        txtCid.Text=cId;
+                        txtGid.Text=strgid;
+                        return;
+                    }
+                    else if (dres == DialogResult.No)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
